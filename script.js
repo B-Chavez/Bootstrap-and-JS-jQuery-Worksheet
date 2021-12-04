c3b32535850a7d0a068b53f952ef66ce78e78dda
Brandon Chavez <brandedhard@gmail.com>
2021-12-03 18:02:42 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+let mainBoxLeft = $('#mainBoxLeft');
+let mainBoxCenter = $('#mainBoxCenter');
+let mainBoxRight = $('#mainBoxRight');
+
+let buttonChange1 = $('#change1');
+let buttonChange2 = $('#change2');
+let buttonChange3 = $('#change3');
+let buttonChange4 = $('#change4');
+
+let buttonHide1 = $('#hide1');
+let buttonHide2 = $('#hide2');
+let buttonHide3 = $('#hide3');
+let buttonHide4 = $('#hide4');
+
+let buttonReset = $('#reset');
+let buttonShowAll = $('#showAll');
+
+buttonChange1.addEventListener('click', changeBoxColor);
+
+function changeBoxColor(){
+  console.log('test');
+}
